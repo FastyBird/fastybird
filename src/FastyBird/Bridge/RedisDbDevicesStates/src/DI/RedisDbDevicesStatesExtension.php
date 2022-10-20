3833b901df12307faad71a8a780994605f430d9c
Adam Kadlec <adam.kadlec@gmail.com>
2022-10-20 23:27:39 +0200

    Creating redis db devices states manager (#7)
    * Created redis db devices states storage bridge
    
    * Added tests
    
    * QA fix

---
+++ src/FastyBird/Bridge/RedisDbDevicesStates/src/DI/RedisDbDevicesStatesExtension.php
@@ -0,0 +1,107 @@
+<?php declare(strict_types = 1);
+
+/**
+ * FbMqttExtension.php
+ *
+ * @license        More in LICENSE.md
+ * @copyright      https://www.fastybird.com
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ * @package        FastyBird:RedisDbDevicesStatesBridge!
+ * @subpackage     DI
+ * @since          0.1.0
+ *
+ * @date           03.12.20
+ */
+
+namespace FastyBird\Bridge\RedisDbDevicesStates\DI;
+
+use FastyBird\Bridge\RedisDbDevicesStates\Models;
+use Nette;
+use Nette\DI;
+use Nette\Schema;
+use stdClass;
+use function assert;
+
+/**
+ * Redis DB devices states storage extension
+ *
+ * @package        FastyBird:RedisDbDevicesStatesBridge!
+ * @subpackage     DI
+ *
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ */
+class RedisDbDevicesStatesExtension extends DI\CompilerExtension
+{
+
+	public const NAME = 'fbRedisDbDevicesStatesBridge';
+
+	public static function register(
+		Nette\Configurator $config,
+		string $extensionName = self::NAME,
+	): void
+	{
+		$config->onCompile[] = static function (
+			Nette\Configurator $config,
+			DI\Compiler $compiler,
+		) use ($extensionName): void {
+			$compiler->addExtension($extensionName, new RedisDbDevicesStatesExtension());
+		};
+	}
+
+	public function getConfigSchema(): Schema\Schema
+	{
+		return Schema\Expect::structure([
+			'database' => Schema\Expect::int(0),
+		]);
+	}
+
+	public function loadConfiguration(): void
+	{
+		$builder = $this->getContainerBuilder();
+		$configuration = $this->getConfig();
+		assert($configuration instanceof stdClass);
+
+		$builder->addDefinition(
+			$this->prefix('models.connectorPropertyRepository'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\ConnectorPropertiesRepository::class)
+			->setArguments(['database' => $configuration->database]);
+
+		$builder->addDefinition(
+			$this->prefix('models.devicePropertyRepository'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\DevicePropertiesRepository::class)
+			->setArguments(['database' => $configuration->database]);
+
+		$builder->addDefinition(
+			$this->prefix('models.channelPropertyRepository'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\ChannelPropertiesRepository::class)
+			->setArguments(['database' => $configuration->database]);
+
+		$builder->addDefinition(
+			$this->prefix('models.connectorPropertiesManager'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\ConnectorPropertiesManager::class)
+			->setArguments(['database' => $configuration->database]);
+
+		$builder->addDefinition(
+			$this->prefix('models.devicePropertiesManager'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\DevicePropertiesManager::class)
+			->setArguments(['database' => $configuration->database]);
+
+		$builder->addDefinition(
+			$this->prefix('models.channelPropertiesManager'),
+			new DI\Definitions\ServiceDefinition(),
+		)
+			->setType(Models\ChannelPropertiesManager::class)
+			->setArguments(['database' => $configuration->database]);
+	}
+
+}
---
+++ src/FastyBird/Bridge/RedisDbDevicesStates/src/States/ConnectorProperty.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+/**
+ * ConnectorProperty.php
+ *
+ * @license        More in LICENSE.md
+ * @copyright      https://www.fastybird.com
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ * @package        FastyBird:RedisDbDevicesStatesBridge!
+ * @subpackage     States
+ * @since          0.1.0
+ *
+ * @date           20.10.22
+ */
+
+namespace FastyBird\Bridge\RedisDbDevicesStates\States;
+
+use FastyBird\Module\Devices\States as DevicesStates;
+
+/**
+ * Device property state
+ *
+ * @package        FastyBird:RedisDbDevicesStatesBridge!
+ * @subpackage     States
+ *
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ */
+class ConnectorProperty extends Property implements DevicesStates\ConnectorProperty
+{
+
+}
