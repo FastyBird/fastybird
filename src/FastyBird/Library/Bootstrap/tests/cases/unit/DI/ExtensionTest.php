0556a5e39649c06d11294ad21fa972f71a681552
Adam Kadlec <adam.kadlec@gmail.com>
2022-11-04 13:13:21 +0100

    Test fix (#23)

--- src/FastyBird/Library/Bootstrap/tests/cases/unit/DI/ExtensionTest.php
+++ src/FastyBird/Library/Bootstrap/tests/cases/unit/DI/ExtensionTest.php
@@ -3,14 +3,20 @@
 namespace FastyBird\Library\Bootstrap\Tests\Cases\Unit\DI;
 
 use FastyBird\Library\Bootstrap\Boot;
+use FastyBird\Library\Bootstrap\Exceptions;
 use FastyBird\Library\Bootstrap\Helpers;
 use Monolog;
+use Nette;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bridge\Monolog as SymfonyMonolog;
 
 final class ExtensionTest extends TestCase
 {
 
+	/**
+	 * @throws Exceptions\InvalidArgument
+	 * @throws Nette\DI\MissingServiceException
+	 */
 	public function testCompilersServices(): void
 	{
 		$configurator = Boot\Bootstrap::boot();
