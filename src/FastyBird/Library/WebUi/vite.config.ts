d92aecf7374f180475c51b4624b68fbbc4e68324
Adam Kadlec <adam.kadlec@gmail.com>
2022-11-07 22:50:46 +0100

    JS cleaning

---
+++ src/FastyBird/Library/WebUi/vite.config.ts
@@ -0,0 +1,44 @@
+import {resolve} from 'path';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        eslintPlugin(),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        },
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/entry.ts'),
+            name: 'web-ui-library',
+            fileName: (format) => `web-ui-library.${format}.js`,
+        },
+        rollupOptions: {
+            external: [
+                'vue',
+            ],
+            output: {
+                sourcemap: true,
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                },
+                assetFileNames: (chunkInfo) => {
+                    if (chunkInfo.name == 'style.css')
+                        return 'web-ui-library.css';
+
+                    return chunkInfo.name as string;
+                },
+            },
+        },
+        sourcemap: true,
+    },
+});
