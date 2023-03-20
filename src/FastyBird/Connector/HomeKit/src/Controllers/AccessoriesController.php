1715d227a2b17c363521af23b8789404fa93e9d6
Adam Kadlec <adam.kadlec@gmail.com>
2023-03-20 23:50:20 +0100

    Fixing config

--- src/FastyBird/Connector/HomeKit/src/Controllers/AccessoriesController.php
+++ src/FastyBird/Connector/HomeKit/src/Controllers/AccessoriesController.php
@@ -33,6 +33,7 @@ use Psr\Http\Message;
 use Ramsey\Uuid;
 use function boolval;
 use function strval;
+use function var_dump;
 
 /**
  * Accessories controller
@@ -87,6 +88,7 @@ final class AccessoriesController extends BaseController
 
 		$result = $this->accessoriesDriver->toHap($connectorId);
 
+		var_dump(Utils\Json::encode($result));
 		$response = $response->withStatus(StatusCodeInterface::STATUS_OK);
 		$response = $response->withHeader('Content-Type', Servers\Http::JSON_CONTENT_TYPE);
 		$response = $response->withBody(SlimRouter\Http\Stream::fromBodyString(Utils\Json::encode($result)));
