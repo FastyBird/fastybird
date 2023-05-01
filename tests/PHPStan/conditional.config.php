f6a4be787121a26c5c6db3d274911bdf543d9332
Adam Kadlec <adam.kadlec@gmail.com>
2023-05-01 11:05:58 +0200

    Added condition

---
+++ tests/PHPStan/conditional.config.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+$config = [];
+
+if (PHP_VERSION_ID >= 8_20_00) {
+	// Change of signature in PHP 8.2
+	$config['parameters']['ignoreErrors'][] = [
+		'message' => '~Method FastyBird\Library\Bootstrap\Helpers\Database::reconnect() has Doctrine\DBAL\Exception in PHPDoc @throws tag but it\'s not thrown.~',
+		'path' => '../../src/FastyBird/Library/Bootstrap/src/Helpers/Database.php',
+		'count' => 1,
+	];
+}
+
+return $config;
