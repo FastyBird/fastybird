d115e60a5db011a6e7990dad4a299fe9217b6f64
Adam Kadlec <adam.kadlec@gmail.com>
2023-05-01 11:18:23 +0200

    Updating check

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 $config = [];
 
-if (PHP_VERSION_ID >= 8_20_00) {
+if (PHP_VERSION_ID >= 8_02_00) {
 	// Change of signature in PHP 8.2
 	$config['parameters']['ignoreErrors'][] = [
 		'message' => '~Method FastyBird\Library\Bootstrap\Helpers\Database::reconnect() has Doctrine\DBAL\Exception in PHPDoc @throws tag but it\'s not thrown.~',
