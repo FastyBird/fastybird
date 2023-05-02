1b99c280e10747661c9e074bfef13d63999c2b46
Adam Kadlec <adam.kadlec@gmail.com>
2023-05-02 23:56:21 +0200

    [general] Updating GA config (#108)
    * Updating GA config
    
    * Removed deprecate code
    
    * Added condition
    
    * Updating check
    
    * Updating
    
    * Updating config
    
    * Config
    
    * Config
    
    * Config
    
    * Config
    
    * Config
    
    * Config
    
    * config
    
    * Update config in all packages
    
    * Fixing qa
    
    * Revert version

---
+++ tests/PHPStan/conditional.config.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+$config = [];
+
+if (PHP_VERSION_ID >= 8_02_00) {
+	// Change of signature in PHP 8.2
+	$config['parameters']['ignoreErrors'][] = [
+		// phpcs:ignore SlevomatCodingStandard.Files.LineLength.LineTooLong
+		'message' => '~Method FastyBird\\\\Library\\\\Bootstrap\\\\Helpers\\\\Database::reconnect\(\) has Doctrine\\\\DBAL\\\\Exception in PHPDoc @throws tag but it\'s not thrown.~',
+		'path' => __DIR__ . '/../../src/FastyBird/Library/Bootstrap/src/Helpers/Database.php',
+		'count' => 1,
+	];
+}
+
+return $config;
