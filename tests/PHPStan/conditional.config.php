fc5c8eaa257713332fa98d04d90d55251a4a405a
Adam Kadlec <adam.kadlec@gmail.com>
2023-06-16 22:13:06 +0200

    QA fix

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -2,6 +2,8 @@
 
 $config = [];
 
+/*
+ * Example:
 if (PHP_VERSION_ID >= 8_02_00) {
 	// Change of signature in PHP 8.2
 	$config['parameters']['ignoreErrors'][] = [
@@ -11,5 +13,6 @@ if (PHP_VERSION_ID >= 8_02_00) {
 		'count' => 1,
 	];
 }
+*/
 
 return $config;
