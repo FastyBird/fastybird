52aef6d5544c5cd0dd98fe8514204f2e71e41c28
Adam Kadlec <adam.kadlec@gmail.com>
2024-01-04 11:21:07 +0100

    QA fixes

--- src/FastyBird/Connector/Zigbee2Mqtt/tests/cases/unit/Clients/DiscoveryTest.php
+++ src/FastyBird/Connector/Zigbee2Mqtt/tests/cases/unit/Clients/DiscoveryTest.php
@@ -24,9 +24,7 @@ use React;
 use React\EventLoop;
 use RuntimeException;
 use function array_diff;
-use function file_exists;
 use function in_array;
-use function is_array;
 use function sprintf;
 
 final class DiscoveryTest extends Tests\Cases\Unit\DbTestCase
