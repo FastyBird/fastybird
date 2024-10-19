6a1141ecda65a3d3b740b2aba58f122ccf6c7ff8
Adam Kadlec <adam.kadlec@gmail.com>
2024-10-19 13:23:39 +0200

    Fixing namespace

--- src/FastyBird/Connector/NsPanel/tests/cases/unit/Mapping/BuilderTest.php
+++ src/FastyBird/Connector/NsPanel/tests/cases/unit/Mapping/BuilderTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace FastyBird\Connector\NsPanel\Tests\Cases\Unit\DI;
+namespace FastyBird\Connector\NsPanel\Tests\Cases\Unit\Mapping;
 
 use Error;
 use FastyBird\Connector\NsPanel\Exceptions;
